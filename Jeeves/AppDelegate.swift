2519150b90c1c4f17b635ad072abc80bc394d242
Sash Zats <sash@zats.io>
2015-03-28 19:48:47 -0700

    initial project

---
+++ Jeeves/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Jeeves
+//
+//  Created by Sash Zats on 3/28/15.
+//  Copyright (c) 2015 Sash Zats. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Jeeves/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jeeves
+//
+//  Created by Sash Zats on 3/28/15.
+//  Copyright (c) 2015 Sash Zats. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
