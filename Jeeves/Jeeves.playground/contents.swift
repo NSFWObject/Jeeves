943fdb4cb79e2c15a04851ac344d2ed549600d3b
Sash Zats <sash@zats.io>
2015-04-07 08:44:37 -0700

    a weird case of retaining functions

--- Jeeves/Jeeves.playground/contents.swift
+++ Jeeves/Jeeves.playground/contents.swift
@@ -1,9 +1,38 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
 import Foundation
 
-let string = "cats"
-let regex = NSRegularExpression(pattern: "^cat$", options: .CaseInsensitive, error: nil)!
-let matches = regex.matchesInString(string, options: NSMatchingOptions.Anchored, range: NSMakeRange(0, (string as NSString).length)) as! [NSTextCheckingResult]
-matches.first!.range
+class TempNotifier {
+    
+    var onChange: (Int) -> Void = {t in }
+    var currentTemp = 72
+    
+    init() {
+        // 1.
+        onChange = { [unowned self] temp in
+            self.currentTemp = temp
+        }
+
+        // 2.
+        onChange = {[unowned self] temp in
+            self.tempHandler(temp)
+        }
+        
+        // 3.
+        unowned let s = self
+        onChange = s.tempHandler
+
+    }
+    
+    deinit {
+        println("deinit")
+    }
+    
+    private func tempHandler(temp: Int) {
+        self.currentTemp = temp
+    }
+}
+
+var tN: TempNotifier? = TempNotifier()
+tN = nil
+
